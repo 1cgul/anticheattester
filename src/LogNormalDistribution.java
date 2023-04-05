ebb6cd4b7d9db2ac75cb3e56d933ea76861022a6
1cgul <cgulino03@gmail.com>
2023-04-05 01:31:49 -0400

    Added a class called LogNormalDistribution which returns a random point on the x axis from a lognormal curve. It will not return a completely random point within the min and max bounds, but will return a random point with respect to the median (mu) and intensity (sigma). This will be used for wait timers instead of using completely random waits as using completely random waits is non human-like.

---
+++ src/LogNormalDistribution.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class LogNormalDistribution {
+
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+        int minX = 180;
+        int maxX = 1500;
+        double mu = Math.log(275);
+        double sigma = 0.425;
+
+        int randomX = generateRandomX(minX, maxX, mu, sigma);
+        System.out.println("Random x value: " + randomX);
+    }
+
+    public static int generateRandomX(int minX, int maxX, double mu, double sigma) {
+        int randomX;
+        do {
+            double gaussianValue = random.nextGaussian() * sigma + mu;
+            randomX = (int) Math.round(Math.exp(gaussianValue));
+        } while (randomX < minX || randomX > maxX);
+
+        return randomX;
+    }
+}
